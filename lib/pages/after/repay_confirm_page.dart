fbe455ff2c6c0d5fcfa7432d93d1f487f2cb5327
dongyu <zhoujin1316@163.com>
2025-09-19 15:43:50 +0800

    审核版专项-删除中文字符串

--- lib/pages/after/repay_confirm_page.dart
+++ lib/pages/after/repay_confirm_page.dart
@@ -6,9 +6,6 @@ class RepayConfirmPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('还款管理')),
-      body: const Center(child: Text('还款页面 - 待实现')),
-    );
+    return const Placeholder();
   }
 }
